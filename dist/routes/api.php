e5f3d7d996bf9b0440cdb9ada20c8f09a0c28d0f
Игорь Анисимов <ia@devspark.ru>
2020-06-02 11:15:47 +0400

    fix

--- dist/routes/api.php
+++ dist/routes/api.php
@@ -4,4 +4,6 @@ use Illuminate\Support\Facades\Route;
 
 $routeData = config('sso.get_logged_user_route');
 
-Route::get($routeData['uri'], $routeData['class'] . '@' . $routeData['method']);
+if (is_array($routeData)) {
+    Route::get($routeData['uri'], $routeData['class'] . '@' . $routeData['method']);
+}
